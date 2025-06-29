9b0aeb6a845679f1d780708f7efb4b780bf0e5ac
Sitan Doucara <doucarasitan@gmail.com>
2025-06-29 20:10:02 +0200

    fix versel build error timezone

--- api/timezone.ts
+++ api/timezone.ts
@@ -1,9 +1,9 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import * as tzLookup from "tz-lookup";
+import tzLookup from "tz-lookup";
 import moment from "moment-timezone";
 
 function getTimezoneFromCoords(lat: number, lon: number): string {
-  return (tzLookup as any)(lat, lon);
+  return tzLookup(lat, lon);
 }
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
